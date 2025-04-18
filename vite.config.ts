d86c61aa341f07ea72b7ab6abb9692d2e6b2a1e1
Anson Ng <914anson5@gmail.com>
2025-04-18 16:51:35 +0800

    [build] add Tailwind CSS and Vue Devtools to project
    - Installed @tailwindcss/vite and tailwindcss dependencies
    - Installed vite-plugin-vue-devtools for Vue debugging
    - Updated vite.config.ts to include Tailwind CSS and Vue Devtools plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevtools from "vite-plugin-vue-devtools";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), vueDevtools(), tailwindcss()],
+});
