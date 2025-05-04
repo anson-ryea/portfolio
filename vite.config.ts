7a4a9374fcd3b9a70b66b86852682278985e05f8
Anson Ng <914anson5@gmail.com>
2025-05-04 15:47:51 +0800

    [build] update dependencies and add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,19 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevtools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevtools(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@Assets": path.resolve(__dirname, "/src/assets"),
+      "@Components": path.resolve(__dirname, "/src/components"),
+      "@Layouts": path.resolve(__dirname, "/src/layouts"),
+      "@Locales": path.resolve(__dirname, "/src/locales"),
+      "@Views": path.resolve(__dirname, "/src/views"),
+      "@": path.resolve(__dirname, "/src"),
+    }
+  }
 });
