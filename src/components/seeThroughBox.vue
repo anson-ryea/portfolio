b6d1c78274f91e07207155a9cad1aade01b2920f
Anson Ng <914anson5@gmail.com>
2025-05-04 16:05:20 +0800

    [feat] add multiple components：Polaroid, SeeThroughBox, Tooltip and ConsoleLikePane

---
+++ src/components/seeThroughBox.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="rounded border border-gray-300 bg-seethrough p-4 font-serif text-gray-600 text-justify">
+        <slot />
+    </div>
+    <div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
