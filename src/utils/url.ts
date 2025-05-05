2c95045def12b14ebe707a3849527f6811081e05
Anson Ng <914anson5@gmail.com>
2025-05-06 01:26:57 +0800

    [fix]: update the routing history to include the base path, and refactor the getAsset function to support multi-level resource paths.

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,3 +1,16 @@
-export default function getAsset(url: string): string {
-    return new URL(`../assets/${url}`, import.meta.url).href
+export default function getAsset(path: string) {
+    const pathArr = path.split('/');
+    switch (pathArr.length) {
+        case 1:
+            return new URL(`/src/assets/${path}`, import.meta.url).href;
+        case 2:
+            return new URL(`/src/assets/${pathArr[0]}/${pathArr[1]}`, import.meta.url).href;
+        case 3:
+            return new URL(
+                `/src/assets/${pathArr[0]}/${pathArr[1]}/${pathArr[2]}`,
+                import.meta.url
+            ).href;
+        default:
+            throw new Error('Invalid path');
+    }
 }
