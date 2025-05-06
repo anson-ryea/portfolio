030e25305a426b1de7ab497cdc9ec253916dbbc9
Anson Ng <914anson5@gmail.com>
2025-05-06 09:41:17 +0800

    [build] update homepage URL and remove base path configuration

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,16 +1,16 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from "vue-router";
 
-const Index = () => import('@Views/index.vue')
-const About = () => import('@Views/about.vue')
+const Index = () => import("@Views/index.vue");
+const About = () => import("@Views/about.vue");
 
 const routes = [
-    { path: '/', name: "Index", component: Index },
-    { path: '/about', name: "About", component: About },
-]
+    { path: "/", name: "Index", component: Index },
+    { path: "/about", name: "About", component: About },
+];
 
 const router = createRouter({
-    history: createWebHistory("/portfolio"),
+    history: createWebHistory(),
     routes,
-})
+});
 
-export default router
+export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,16 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio",
-  plugins: [vue(), vueDevtools(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@Assets": path.resolve(__dirname, "/src/assets"),
-      "@Components": path.resolve(__dirname, "/src/components"),
-      "@Layouts": path.resolve(__dirname, "/src/layouts"),
-      "@Locales": path.resolve(__dirname, "/src/locales"),
-      "@Stores": path.resolve(__dirname, "/src/stores"),
-      "@Views": path.resolve(__dirname, "/src/views"),
-      "@": path.resolve(__dirname, "/src"),
-    }
-  }
+    plugins: [vue(), vueDevtools(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@Assets": path.resolve(__dirname, "/src/assets"),
+            "@Components": path.resolve(__dirname, "/src/components"),
+            "@Layouts": path.resolve(__dirname, "/src/layouts"),
+            "@Locales": path.resolve(__dirname, "/src/locales"),
+            "@Stores": path.resolve(__dirname, "/src/stores"),
+            "@Views": path.resolve(__dirname, "/src/views"),
+            "@": path.resolve(__dirname, "/src"),
+        },
+    },
 });
