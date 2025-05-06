27a1308b6a4a07e8e61520db812da4202f1a5360
Anson Ng <914anson5@gmail.com>
2025-05-06 10:26:22 +0800

    [prod]: add Vercel analytics and speed insights components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <template>
+    <SpeedInsights />
+    <Analytics />
     <Suspense>
         <RouterView class="h-full w-full max-w-screen" />
     </Suspense>
@@ -7,6 +9,8 @@
 </template>
 
 <script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/vue';
+import { Analytics } from '@vercel/analytics/vue';
 import Footer from "@Layouts/footer.vue"
 import navBar from "@Layouts/navBar.vue"
 </script>
