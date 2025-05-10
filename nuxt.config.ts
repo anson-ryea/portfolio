3a528b343b00916e9b3d098649d4a82752846222
Anson Ng <914anson5@gmail.com>
2025-05-11 00:10:19 +0800

    [feat] update website icons and web manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,11 @@ export default defineNuxtConfig({
     app: {
         head: {
             title: "Anson Ng",
-            link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+            link: [{ rel: "icon", type: "image/png", href: "/favicon-96x96.png", sizes: "96x96" },
+            { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+            { rel: "shortcut icon", href: "/favicon.ico" },
+            { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+            { rel: "manifest", href: "/site.webmanifest" }],
         },
         pageTransition: { name: "page", mode: "out-in" },
     },
