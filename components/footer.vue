4673dfa256967b3ddc2a72cf4bbd39cd81edba97
Anson Ng <914anson5@gmail.com>
2025-05-16 19:20:49 +0800

    [feat] implement localization for footer, navbar, and pages; update translations

--- components/footer.vue
+++ components/footer.vue
@@ -4,11 +4,10 @@
             class="flex flex-col md:mx-8 lg:mx-12 xl:mx-20 flex-1 bg-white border-x border-gray-300 border-dashed hover:border-solid divide-y divide-gray-300 divide-dashed">
             <FooterSection class="flex justify-between items-center">
                 <div>
-                    <h6 class="font-semibold">Get in touch</h6>
-                    <p class="font-medium text-gray-500">Send me an email.</p>
+                    <h6 class="font-semibold">{{ $t("footer.contact.title") }}</h6>
+                    <p class="font-medium text-gray-500">{{ $t("footer.contact.description") }}</p>
                 </div>
                 <NuxtLink :to="`mailto:${$t('info.emailAddress')}`" external class="flex btn-light items-center gap-2">
-
                     {{ $t("info.emailAddress") }}
                     <Icon name="solar:plain-2-broken" class="h-4 w-4 bg-gray-600" />
                 </NuxtLink>
@@ -19,16 +18,22 @@
             </FooterSection>
             <FooterSection
                 class="flex flex-col md:flex-row md:items-end md:justify-between text-gray-600 font-mono text-sm text-center pb-12">
-                <p class="md:text-left">
-                    Iteration {{ iteration }} Patch {{ minor }} <br />All rights
-                    reserved © {{ year }} {{ $t("info.url") }}
-                </p>
-                <p class="md:text-right">
-                    Made with <span class="text-green-400">Nuxt</span> and
-                    <span class="text-sky-400">Tailwind CSS</span>
-                    <br />
-                    Designed and engineered by Anson Ng
-                </p>
+                <div class="md:text-left">
+                    <p class="capitalize">
+                        {{ $t("footer.version.majorLabel") }} {{ iteration }} {{ $t("footer.version.patchLabel") }} {{
+                            minor
+                        }}
+                    </p>
+                    <p>{{ $t("footer.copyright.description") }} {{ year }} {{ $t("info.url") }}</p>
+                </div>
+                <div class="md:text-right">
+                    <p>
+                        {{ $t("footer.copyright.madeWith") }} <span class="text-green-600">Nuxt</span> {{
+                            $t("dictionary.and") }}
+                        <span class="text-sky-400">Tailwind CSS</span>
+                    </p>
+                    <p>{{ $t("footer.copyright.credit") }}</p>
+                </div>
             </FooterSection>
         </div>
     </footer>
