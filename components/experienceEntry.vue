ef12063cf509068c82dd77c584f1abed66711017
Anson Ng <914anson5@gmail.com>
2025-05-21 13:57:42 +0800

    [fix] fix incorrect comparison for isPresent in experienceEntry

--- components/experienceEntry.vue
+++ components/experienceEntry.vue
@@ -1,18 +1,26 @@
 <template>
-    <div class="max-md:space-y-2 md:flex md:divide-x md:divide-gray-300 md:divide-dashed">
-        <div class="flex flex-col max-md:space-y-2 md:justify-between md:w-36 lg:w-48 xl:w-64 md:pr-4">
+    <div
+        class="max-md:space-y-2 md:flex md:divide-x md:divide-gray-300 md:divide-dashed"
+    >
+        <div
+            class="flex flex-col max-md:space-y-2 md:justify-between md:w-36 lg:w-48 xl:w-64 md:pr-4"
+        >
             <div class="max-md:flex text-balance">
-                <span class="font-mono text-xs text-gray-600">{{ startDateString }} ~ {{ endDateString }}</span>
-                <span class="font-mono text-xs text-gray-600 md:hidden">&nbsp·&nbsp</span>
+                <span class="font-mono text-xs text-gray-600"
+                    >{{ startDateString }} ~ {{ endDateString }}</span
+                >
+                <span class="font-mono text-xs text-gray-600 md:hidden"
+                    >&nbsp·&nbsp</span
+                >
                 <h6 class="max-md:text-xs font-mono">
                     {{ experience.location }}
                 </h6>
             </div>
             <div class="flex flex-wrap gap-1">
-                <Tag v-show="isPresent" class="bg-blue-100! text-blue-950!">Present</Tag>
-                <Tag v-for="tag in experience.tags" :key="tag">{{
-                    tag
-                    }}</Tag>
+                <Tag v-show="isPresent" class="bg-blue-100! text-blue-950!"
+                    >Present</Tag
+                >
+                <Tag v-for="tag in experience.tags" :key="tag">{{ tag }}</Tag>
             </div>
         </div>
         <div class="md:pl-4 space-y-2 flex-1">
@@ -27,7 +35,11 @@
                 {{ experience.description }}
             </p>
             <ul class="text-gray-600 list-disc list-inside font-serif">
-                <li v-if="experience.highlights" v-for="highlight in experience.highlights" :key="highlight">
+                <li
+                    v-if="experience.highlights"
+                    v-for="highlight in experience.highlights"
+                    :key="highlight"
+                >
                     {{ highlight }}
                 </li>
             </ul>
@@ -55,18 +67,22 @@ const isPresent = computed(() => {
 });
 
 const startDateString = computed(() => {
-    return new Date(experience.value.startDate).toLocaleDateString(locale.value, {
-        month: "short",
-        year: "numeric",
-    });
+    return new Date(experience.value.startDate).toLocaleDateString(
+        locale.value,
+        {
+            month: "short",
+            year: "numeric",
+        },
+    );
 });
 
 const endDateString = computed(() => {
-    return new Date(experience.value.endDate).getDate() === new Date("2100-01-01").getDate()
+    return new Date(experience.value.endDate).getFullYear() ===
+        new Date("2100-01-01").getFullYear()
         ? t("dictionary.present")
         : new Date(experience.value.endDate).toLocaleDateString(locale.value, {
-            month: "short",
-            year: "numeric",
-        });
+              month: "short",
+              year: "numeric",
+          });
 });
 </script>
