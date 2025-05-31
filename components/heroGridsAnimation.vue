5b6efbe6871e9e87ade554cdca0ef58e4f0abb26
Anson Ng <914anson5@gmail.com>
2025-05-31 21:32:42 +0800

    [refactor] separate hero animation as component

---
+++ components/heroGridsAnimation.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="grid grid-cols-10 h-screen w-full">
+    <motion.div class="bg-blue-600 border border-blue-900 rounded flex-1" v-for="i in 100"
+      :initial="{ opacity: 0.85, filter: `brightness(${(i % 7 * 5 + 30) / 100})` }"
+      :animate="{ opacity: 0, filter: `brightness(1)`, transition: { delay: ((i - 1) % 10 - (i - 1) / 10 + 10) * 0.03 } }"
+      :whileHover="{ opacity: 0.5 }" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { motion } from 'motion-v';
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,7 @@
 <template>
     <div class="flex flex-col w-full justify-between">
-        <div
-            class="relative h-screen w-full bg-[url(/index/hero.jpg)] bg-cover bg-center shadow-2xl select-none grid grid-cols-10">
-            <motion.div class="bg-blue-600 border border-blue-900 rounded" v-for="i in 100"
-                :initial="{ opacity: 0.85, filter: `brightness(${(i % 7 * 5 + 30) / 100})` }"
-                :animate="{ opacity: 0, filter: `brightness(1)`, transition: { delay: ((i - 1) % 10 - (i - 1) / 10 + 10) * 0.03 } }"
-                :whileHover="{ opacity: 0.5 }" />
+        <div class="relative h-screen w-full bg-[url(/index/hero.jpg)] bg-cover bg-center shadow-2xl select-none">
+            <HeroGridsAnimation />
             <motion.div class="absolute bottom-16 right-8 z-10" :initial="{ opacity: 0, scale: 0 }"
                 :animate="{ opacity: 1, scale: 1 }">
                 <h1 class="text-stone-200/50 font-pixel font-bold tracking-[-0.12em]! text-shadow-lg">
