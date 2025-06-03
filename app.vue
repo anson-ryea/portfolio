8069c8d17be62b09e39bf78caae8882440536440
Anson Ng <914anson5@gmail.com>
2025-06-03 19:15:17 +0800

    [fix] hide cursor animation on devices without hover support

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
     </NuxtLayout>
     <Footer />
     <NavBar class="fixed bottom-2 left-1/2 -translate-x-1/2 w-95/100 md:w-fit z-100" />
-    <CursorAnimated class="fixed top-0 z-9999 w-fit" />
+    <CursorAnimated class="fixed top-0 z-9999 w-fit [@media(hover:none)]:hidden" />
 </template>
 
 <script setup lang="ts">
