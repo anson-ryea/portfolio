242edae5731e992add9b42e484e8771375f5ba2b
Anson Ng <914anson5@gmail.com>
2026-07-24 21:32:38 +0800

    [fix] fix incorrect default font for font-sans

--- app/app.vue
+++ app/app.vue
@@ -4,4 +4,14 @@
   </NuxtLayout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useSeoMeta({
+  ogSiteName: "www.an5on.com",
+  title: "Anson Ng",
+  ogTitle: "Anson Ng",
+  description:
+    "Anson Ng is a dedicated BSc Computer Science student at the Chinese University of Hong Kong.",
+  ogDescription:
+    "Anson Ng is a dedicated BSc Computer Science student at the Chinese University of Hong Kong.",
+});
+</script>
