f7f0f93f20ff4fb43b5d15d8e4fb9b8b3efe2c07
Anson Ng <914anson5@gmail.com>
2026-08-05 17:11:24 +0800

    [feat] add academic email

--- app/components/navBarContact.vue
+++ app/components/navBarContact.vue
@@ -1,17 +1,30 @@
 <template>
   <div
-    class="border border-gray-300 rounded-t bg-gray-100/60 backdrop-blur-sm shadow px-2 py-2 font-medium"
+    class="border border-gray-300 rounded-t bg-gray-100/60 backdrop-blur-sm shadow px-2 py-2 space-y-1 font-medium font-mono text-sm text-gray-600"
+    name="fade"
   >
-    <div class="grid grid-cols-6 font-mono text-sm text-gray-600 gap-1" name="fade">
-      <NuxtLink
-        key="email"
-        :to="`mailto:${$t('info.emailAddress')}`"
-        external
-        class="flex items-center gap-2 col-span-3 w-full justify-center btn-light px-2 py-1"
-      >
-        {{ $t("info.emailAddress") }}
-        <Icon name="solar:plain-2-broken" class="h-4 w-4 bg-gray-600 pointer-events-none" />
-      </NuxtLink>
+    <NuxtLink
+      key="email"
+      :to="`mailto:${$t('info.emailAddress')}`"
+      external
+      class="flex items-center gap-2 col-span-1 w-full justify-center btn-light px-2 py-1"
+    >
+      {{ $t("info.emailAddress") }}
+      <Icon name="solar:plain-2-broken" class="h-4 w-4 bg-gray-600 pointer-events-none" />
+    </NuxtLink>
+    <NuxtLink
+      key="email"
+      :to="`mailto:${$t('info.academicEmailAddress')}`"
+      external
+      class="flex items-center gap-2 col-span-1 w-full justify-center btn-light px-2 py-1"
+    >
+      {{ $t("info.academicEmailAddress") }}
+      <Icon
+        name="solar:square-academic-cap-2-broken"
+        class="h-4 w-4 bg-gray-600 pointer-events-none"
+      />
+    </NuxtLink>
+    <div class="grid grid-cols-3 gap-1">
       <NuxtLink
         :to="$t('info.githubUrl')"
         external
